fb98273a822456b75e3f37f1194a89b259daf8df
Polina Nazarova <polina.w.nazarova@gmail.com>
2022-11-21 14:34:52 +0200

    tehtävät 6.3-6.8

---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Ancecdote = ({key, anecdote, handleClick }) => {
+    return (
+        <div key={key}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={handleClick}>vote</button>
+          </div>
+        </div>
+    )
+}
+
+const AnecdoteList = () => {
+
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+
+    
+    return (
+        <div>
+            {anecdotes.map(anecdote =>
+                <Ancecdote 
+                    key={anecdote.id}
+                    anecdote={anecdote}
+                    handleClick={() => 
+                        dispatch(vote(anecdote.id))}
+                />
+            )}
+        </div>
+        
+    )
+}
+
+export default AnecdoteList
