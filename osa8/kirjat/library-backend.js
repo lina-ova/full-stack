c8ce927be7a2c2a09c2bd6f35d7b14fcf958780b
Polina Nazarova <polina.w.nazarova@gmail.com>
2023-01-27 15:02:30 +0200

    8.5

--- osa8/kirjat/library-backend.js
+++ osa8/kirjat/library-backend.js
@@ -98,7 +98,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -109,8 +109,14 @@ const resolvers = {
     authorCount: () => authors.length,
     allBooks: (root, args) => {
       console.log(args)
+      if (args.author && args.genre) {
+        return books.filter( book => book.author == args.author && book.genres.includes(args.genre) )
+      }
       if (args.author ) {
         return books.filter(book => book.author === args.author)
+      }
+      if (args.genre) {
+        return books.filter( book => book.genres.includes(args.genre))
       }
       return books
     },
