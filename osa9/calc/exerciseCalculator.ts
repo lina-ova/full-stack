23827bfa6190d83525ad0e17ee8dfdf8768155cb
Polina Nazarova <polina.w.nazarova@helsinki.fi>
2023-03-04 17:13:24 +0200

    8.3

--- osa9/calc/exerciseCalculator.ts
+++ osa9/calc/exerciseCalculator.ts
@@ -1,3 +1,7 @@
+interface TrainValues {
+  target: number;
+  exercises: number[];
+}
 interface Training { 
   periodLength: number,
   trainingDays: number,
@@ -7,8 +11,20 @@ interface Training {
   target: number,
   average: number }
 
+const Arguments = (args: string[]): TrainValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[2])) && args.slice(3).every(e => !isNaN(Number(e)))) {
+    return {
+      target: Number(args[2]),
+      exercises: args.slice(3).map(a => Number(a))
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateExercises = (daily: number[], target:number) :Training=> {
-  let yours : Training
   let periodLength = daily.length
   let trainingDays = daily.filter(e => e >0).length
   let average = daily.reduce((a, b)=>(a + b))/periodLength
@@ -37,4 +53,13 @@ const calculateExercises = (daily: number[], target:number) :Training=> {
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { target, exercises } = Arguments(process.argv);
+  console.log(calculateExercises(exercises, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
